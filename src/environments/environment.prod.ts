1882392aeacafe728c56313cb9caccd777fb7fce
Carl <github@carlpeterschwartz.com>
2022-12-15 21:59:58 +0100

    Initial commit.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiKey: THEMOVIEDB_API_KEY,  // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
+};
