6876d99c52bd540eb65b2eb0051c12f9d2816b2b
Carl <github@carlpeterschwartz.com>
2022-12-16 10:24:46 +0100

    Added process.env to test Netlify environment variable.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: true,
-  apiKey: API_KEY_THEMOVIEDB,  // <-- Enter your own key here!'
+  apiKey: process.env.API_KEY_THEMOVIEDB,  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
