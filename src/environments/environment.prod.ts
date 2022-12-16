813debc67871c7defbdac2980e8ee6a25ab5292f
Carl <github@carlpeterschwartz.com>
2022-12-16 16:42:59 +0100

    Added process.env to environment apiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 //import apiKeyTheMovieDb from 'netlify/functions/themoviedb-netlify'
-const apiKeyTheMovieDb = process.env.API_KEY_THEMOVIEDB;
+//const apiKeyTheMovieDb = process.env.API_KEY_THEMOVIEDB;
 
 export const environment = {
   production: true,
-  apiKey: apiKeyTheMovieDb,  // <-- Enter your own key here!'
+  apiKey: process.env.API_KEY_THEMOVIEDB,  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
