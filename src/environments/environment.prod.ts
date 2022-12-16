4e14695094653e46cf2110f46ad7ccf43c63709f
Carl <github@carlpeterschwartz.com>
2022-12-16 17:22:40 +0100

    Added API_KEY_THEMOVIEDB: '', placeholder.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 //import apiKeyTheMovieDb from 'netlify/functions/themoviedb-netlify'
-const apiKeyTheMovieDbProd = process.env['API_KEY_THEMOVIEDB'];
+//const apiKeyTheMovieDbProd = process.env['API_KEY_THEMOVIEDB'];
 
 export const environment = {
   production: true,
-  apiKey: apiKeyTheMovieDbProd,  // <-- Enter your own key here!'
+  API_KEY_THEMOVIEDB: '',  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
