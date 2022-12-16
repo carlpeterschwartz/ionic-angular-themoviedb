4d9e8cc248805ce88feb4ed74f6545dabaf63a11
Carl <github@carlpeterschwartz.com>
2022-12-16 11:36:08 +0100

    Testing process.ev without console log

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 //import API_KEY_THEMOVIEDB from 'netlify/functions/'
-let apiKeyTheMovieDb = process.env['API_KEY_THEMOVIEDB'];
+let apiKeyTheMovieDb = process.env.API_KEY_THEMOVIEDB;
 export const environment = {
   production: true,
   apiKey: apiKeyTheMovieDb,  // <-- Enter your own key here!'
