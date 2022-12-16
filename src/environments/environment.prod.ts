64b78fadba20505de5315f391db4031a3a085b15
Carl <github@carlpeterschwartz.com>
2022-12-16 18:14:56 +0100

    Removed comments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
-//import apiKeyTheMovieDb from 'netlify/functions/themoviedb-netlify'
-//const apiKeyTheMovieDbProd = process.env['API_KEY_THEMOVIEDB'];
-
 export const environment = {
   production: true,
   API_KEY_THEMOVIEDB: 'your_api_key_here',  // <-- Enter your own key here!'
