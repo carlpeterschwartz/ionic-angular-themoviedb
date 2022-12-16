24ab606c240e45fc4d03631356c7c83999538c96
Carl <github@carlpeterschwartz.com>
2022-12-16 17:39:56 +0100

    Updated placeholder text.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 
 export const environment = {
   production: true,
-  API_KEY_THEMOVIEDB: '',  // <-- Enter your own key here!'
+  API_KEY_THEMOVIEDB: 'your_api_key_here',  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
