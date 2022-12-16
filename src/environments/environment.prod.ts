1c3cd309ad6ed2b027f97cb72a249ca4581e9f17
Carl <github@carlpeterschwartz.com>
2022-12-16 12:48:47 +0100

    Try again process.env['API_KEY_THEMOVIEDB']

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 
 export const environment = {
   production: true,
-  apiKey: apiKeyTheMovieDb,  // <-- Enter your own key here!'
+  apiKey: process.env['API_KEY_THEMOVIEDB'],  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
