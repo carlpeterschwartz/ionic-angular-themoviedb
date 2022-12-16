063c86b92801145b2cfd5cc058e79bb464b50696
Carl <github@carlpeterschwartz.com>
2022-12-16 19:27:52 +0100

    Capitalized all environment variables.

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -10,7 +10,7 @@ import { environment } from 'src/environments/environment';
 })
 export class MovieDetailsPage implements OnInit {
   movie:any = null;
-  imageBaseUrl = environment.images;
+  imageBaseUrl = environment.IMAGES;
 
   constructor(
     private route: ActivatedRoute,
