1f8d4239dc08ee8d97bc4fad2c45d73e0dfd3ca0
Carl <github@carlpeterschwartz.com>
2022-12-16 13:16:33 +0100

    Removed process.env from environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 //import API_KEY_THEMOVIEDB from 'netlify/functions/'
-//let apiKeyTheMovieDb = process.env["API_KEY_THEMOVIEDB"];
+let apiKeyTheMovieDb = process.env['API_KEY_THEMOVIEDB'];
 
 export const environment = {
   production: true,
-  apiKey: process.env['API_KEY_THEMOVIEDB'],  // <-- Enter your own key here!'
+  apiKey: apiKeyTheMovieDb,  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
