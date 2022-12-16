e8b659987c892576bd4e182183cf1ef7a2a866f3
Carl <github@carlpeterschwartz.com>
2022-12-16 13:23:35 +0100

    Hard import from environment.prod

--- netlify/functions/themoviedb-netlify.ts
+++ netlify/functions/themoviedb-netlify.ts
@@ -1,1 +1,1 @@
-const apiKeyTheMovieDb = process.env['API_KEY_THEMOVIEDB'];
+//const apiKeyTheMovieDb = process.env['API_KEY_THEMOVIEDB'];
--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
 import { MovieService } from 'src/app/services/movie.service';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-movies',
