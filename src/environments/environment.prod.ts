a13b22217d708c380e9438e544edd449a2ab5eca
Carl <github@carlpeterschwartz.com>
2022-12-16 11:25:35 +0100

    More testing for Netlify Enviroment Variable.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 //import API_KEY_THEMOVIEDB from 'netlify/functions/'
-
+let apiKeyTheMovieDb = process.env['API_KEY_THEMOVIEDB'];
 export const environment = {
   production: true,
-  apiKey: process.env.API_KEY_THEMOVIEDB,  // <-- Enter your own key here!'
+  apiKey: apiKeyTheMovieDb,  // <-- Enter your own key here!'
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'http://image.tmdb.org/t/p',
 };
